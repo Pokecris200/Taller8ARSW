e32934a895c0c0f9873cab0b4d1e9973e339744d
unknown <cristian.forero-m@labinfo.is.escuelaing.edu.co>
2022-07-07 12:21:25 -0500

    Inicio Generación de llaves

---
+++ src/main/java/co/edu/escuelaing/interactivebalckboardlife/UserMemory.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.escuelaing.interactivebalckboardlife;
+
+import java.util.List;
+
+/**
+ *
+ * @author cristian.forero-m
+ */
+public class UserMemory {
+    private static List<Integer> instance;
+}
