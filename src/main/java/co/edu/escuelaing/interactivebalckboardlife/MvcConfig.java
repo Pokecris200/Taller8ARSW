a42f844988878e1dbc5ce3481f3ef70aecd31f37
unknown <cristian.forero-m@labinfo.is.escuelaing.edu.co>
2022-07-07 11:13:10 -0500

    Añadidos elementos de seguridad basica

---
+++ src/main/java/co/edu/escuelaing/interactivebalckboardlife/MvcConfig.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.escuelaing.interactivebalckboardlife;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+        @Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/bb2").setViewName("bb2");
+		registry.addViewController("/login").setViewName("login");
+	}
+
+}
